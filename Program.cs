b0c6e7ff0c1c8d722d5f077c88fb468e9ce7e806
Abdalhakim123 <70113116+Abdalhakim123@users.noreply.github.com>
2020-11-27 08:58:14 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Äpplen ?:");
+            int Applen = int.Parse(Console.ReadLine());
+            Console.WriteLine("Päron ?:");
+            int Paron = int.Parse(Console.ReadLine());
+
+            Applen = Applen * 7;
+            Paron = Paron * 13;
+
+            if (Paron < Applen)
+            {
+                Console.WriteLine("Axel");
+            }
+            else if (Paron > Applen)
+            {
+                Console.WriteLine("Petra");
+            }
+            else
+            {
+                Console.WriteLine("Lika");
+            }
+        }
+    }
+}
